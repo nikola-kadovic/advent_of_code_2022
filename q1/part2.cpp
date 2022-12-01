7141fc58758bca190fa0205dfd5a5bb5a8ab2259
Nikola Kadovic <kadovicnikola@gmail.com>
2022-12-01 12:31:38 -0500

    improvement

--- q1/part2.cpp
+++ q1/part2.cpp
@@ -4,25 +4,23 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	vector<ll> ans(3);
+	set<ll> s;
 	ll cur = 0;
 	string line;	
 
 	while(getline(cin, line)) {
 		if(line == "") {
-			for(int i = 0; i < 3; i++) {
-				if(cur > ans[i]) {
-					ans.insert(ans.begin()+i, 1, cur);
-					ans.pop_back();
-					break;
-				}
-			}
+			s.insert(cur);
+			if(s.size() > 3) s.erase(s.begin());
 			cur = 0;
 		}else {
 			cur += stol(line, nullptr, 10);
 		}
 	}
+	ll ans = 0;
+	for(auto it : s) 
+		ans += it;
 
-	cout << ans[0] + ans[1] + ans[2] << "\n";
+	cout << ans << "\n";
 	return 0;	
 }
